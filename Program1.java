baa4f4298bde59920a3db7760f3b85c0fb926928
XDen78 <venera.opt@gmail.com>
2020-11-17 23:06:18 +0300

    Program

---
+++ Program1.java
@@ -0,0 +1,13 @@
+public class Program
+{
+  public static void main (String [] args)
+  {
+    System.out.println ("Welcom to Zoo");
+    Zebra zebra=new Zebra();
+    zebra.talk();
+    Giraffe giraffe=new Giraffe();
+    giraffe.talk();
+    Lion lion=new Lion();
+    lion.talk();
+  }
+}
